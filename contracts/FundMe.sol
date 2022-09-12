201f3119cf9bfa0cdeae7d624d98341c5445864e
Remix Workspace <>
2022-09-12 18:00:56 +0430

    added require and compared price

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.8;
 
+contract FundMe{
+    uint256 public minimumUsd = 50;
+
+    function fund() public payable{
+        require(msg.value >= minimumUsd , "not enough sent!"); //1e18 = 1eth
+    }
+
+
+    //function withdraw(){}
+}
